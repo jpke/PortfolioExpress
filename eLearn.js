73e4fb012695b39b81deda2767ed6ed1b0f883f3
jpke <j.patrick.earnest@gmail.com>
2017-02-21 16:32:15 -0500

    added comments to code

--- eLearn.js
+++ eLearn.js
@@ -36,25 +36,26 @@ var APP_ID = process.env.BOX_APP_ID;
 var ENTERPRISE_ID = process.env.ENTERPRISE_ID;
 
 var PRIVATE_KEY = fs.readFileSync("private_key.pem", 'utf-8');
-  var sdk = new BoxSDK({
-    clientID: CLIENT_ID,
-    clientSecret: CLIENT_SECRET,
-    appAuth: {
-      keyID: PUBLIC_KEY_ID,
-      privateKey: PRIVATE_KEY,
-      passphrase: PRIVATE_KEY_PASSPHRASE
-    }
-  });
+var sdk = new BoxSDK({
+  clientID: CLIENT_ID,
+  clientSecret: CLIENT_SECRET,
+  appAuth: {
+    keyID: PUBLIC_KEY_ID,
+    privateKey: PRIVATE_KEY,
+    passphrase: PRIVATE_KEY_PASSPHRASE
+  }
+});
 
-  var box = sdk.getAppAuthClient('enterprise', ENTERPRISE_ID);
-
-  box.users.get(box.CURRENT_USER_ID, null, function(err, currentUser) {
-    if(err) console.log("error: ", err);
-  });
+//initialize BOX sdk to access course lessons
+var box = sdk.getAppAuthClient('enterprise', ENTERPRISE_ID);
+box.users.get(box.CURRENT_USER_ID, null, function(err, currentUser) {
+  if(err) console.log("error: ", err);
+});
 
 
 var jsonParser = bodyParser.json()
 
+//create passport strategy to check for valid json web tokens.
 passport.use(new Strategy(
   function(token, done) {
     if(token) {
@@ -70,8 +71,14 @@ passport.use(new Strategy(
   }
 ))
 router.use(passport.initialize())
+
+//serve files form build folder
 router.use(express.static('build'));
 
+//register new user
+//requires name, email and password in request body
+//returns status 201 with mongo id, name, courses (enrolled in) and json web token valide for 24 hours
+//returns status 400 for email already in use, status 500 for server or database error
 router.post('/users', jsonParser, function(req,res) {
   var name = req.body.name
   var email = req.body.email
@@ -164,10 +171,11 @@ router.post('/users', jsonParser, function(req,res) {
   })
 })
 
-// router.get('/users', passport.authenticate('bearer', {session: false}), function(req, res) {
-//   return res.status(200).json({message: "Token validated"})
-// })
-
+//unenroll user from a course
+//must be course admin to submit request
+//requires user email and course id in request body
+//returns status 200 with remaining enrolled courses for user
+//returns status 500 for server or database error
 router.put('/users', jsonParser, passport.authenticate('bearer', {session: false}), function(req, res) {
   var course_id = req.body.course_id;
   var courseInToken = req.user.courses.find(function(courseInToken) {
@@ -196,6 +204,9 @@ router.put('/users', jsonParser, passport.authenticate('bearer', {session: false
 
 
 //render certificate of completion pdf, send to client
+//requires course id and json web token to be included in request url
+//returns status 200 with rendered pdf
+//returns status 401 for invalid token
 router.get('/users/certificate/:course/:token', function(req, res) {
   if(req.params.token) {
     jwt.verify(req.params.token, TOKENSECRET, function(err, decoded) {
@@ -225,7 +236,10 @@ router.get('/users/certificate/:course/:token', function(req, res) {
   }
 })
 
-
+//logs in user, generates json web token valid for 24 hours
+//requires user email and password in request body
+//returns status 200 with user mongo id, name, email, enrolled courses, ids of passed quizzes and json web token
+//returns status 400 for invalide password or token
 router.post('/login', jsonParser, function(req, res) {
   var password = req.body.password
   UserElearn.findOne({email: req.body.email})
@@ -262,7 +276,7 @@ router.post('/login', jsonParser, function(req, res) {
         course.admin = course.admin.toString().indexOf(user._id) > -1;
         return course;
       })
-      return res.status(302).json({
+      return res.status(200).json({
         _id: user._id,
         name: user.name,
         courses: courses,
@@ -277,6 +291,12 @@ router.post('/login', jsonParser, function(req, res) {
   });
 })
 
+//querys a course for enrolled and enrollable users
+//requires course id in request url
+//must be course admin to make request
+//returns status 200 with list of enrolled users and enrollable unregistered user emails
+//returns status 401 if request is not made by course admin
+//returns status 500 for server or database error
 router.get('/course/enrollable/:course_id', passport.authenticate('bearer', {session: false}), function(req, res) {
   var course_id = req.params.course_id;
   var courseInToken = req.user.courses.find(function(courseInToken) {
@@ -302,6 +322,12 @@ router.get('/course/enrollable/:course_id', passport.authenticate('bearer', {ses
   });
 });
 
+//adds unregistered user email to list of emails enrollable in a course
+//must be course admin to make request
+//requires course id, unregistered user email and admin boolean, indicating if user should be listed as a course admin, in request body
+//returns status 201 with updated list of enrollable users for course
+//returns status 401 if request is not made by course admin
+//returns status 500 for server or database error
 router.post('/course/enrollable', jsonParser, passport.authenticate('bearer', {session: false}), function(req, res) {
   var course_id = req.body.course_id;
   var courseInToken = req.user.courses.find(function(courseInToken) {
@@ -325,6 +351,12 @@ router.post('/course/enrollable', jsonParser, passport.authenticate('bearer', {s
   });
 });
 
+//removes unregistered user email from list of emails enrollable in course
+//must be course admin to make request
+//requires course id, and unregistered user email in request body
+//returns status 200 with updated list of enrollable users for course
+//returns status 401 if request is not made by course admin
+//returns status 500 for server or database error
 router.delete('/course/enrollable', jsonParser, passport.authenticate('bearer', {session: false}), function(req, res) {
   var course_id = req.body.course_id;
   var courseInToken = req.user.courses.find(function(courseInToken) {
@@ -344,6 +376,12 @@ router.delete('/course/enrollable', jsonParser, passport.authenticate('bearer', 
   });
 });
 
+//updates course name
+//must be course admin to make request
+//requires course id and updated course name in request body
+//returns status 200 with updated course data (course quizzes populated with quiz id and title)
+//returns status 401 if request is not made by course admin
+//returns status 500 for server or database error
 router.put('/course', jsonParser, passport.authenticate('bearer', {session: false}), function(req, res) {
   var course = req.body.course;
   var courseInToken = req.user.courses.find(function(courseInToken) {
@@ -366,6 +404,10 @@ router.put('/course', jsonParser, passport.authenticate('bearer', {session: fals
     })
 });
 
+//fetches quiz and any submissions of quiz by user
+//requires quiz id and user id in request url
+//returns status 200 with quiz and prior submissions of quiz by user
+//returns status 500 for server or database error
 router.get('/quiz/:quizId/:userId', passport.authenticate('bearer', {session:false}), function(req, res) {
   Quiz.find({_id: req.params.quizId}).exec()
   .then(function(quiz) {
@@ -385,6 +427,13 @@ router.get('/quiz/:quizId/:userId', passport.authenticate('bearer', {session:fal
   });
 })
 
+//creates or updates quiz; new quiz is listed to course
+//must be course admin to make request
+//requires course id, quiz id, title, courses, items, and minimumScore in request body
+//returns status 201 with created quiz and updated course when quiz created
+//returns status 200 with updated quiz and course when quiz updated
+//returns status 401 if request is not made by course admin
+//returns status 500 for server or database error
 router.put('/quiz', jsonParser, passport.authenticate('bearer', {session: false}), function(req, res) {
   console.log("save quiz request received");
   var course = req.user.courses.find(function(course) {
@@ -442,7 +491,7 @@ router.put('/quiz', jsonParser, passport.authenticate('bearer', {session: false}
                   }).exec()
       .then(function(course) {
         course.admin = true;
-        return res.status(201).json([quiz, course]);
+        return res.status(200).json([quiz, course]);
       })
       .catch(function(err) {
         if(err) console.log("Mongo error: ", err);
@@ -452,6 +501,12 @@ router.put('/quiz', jsonParser, passport.authenticate('bearer', {session: false}
   }
 });
 
+//deletes quiz, and removes quiz listing from course
+//must be course admin to make request
+//requires quiz id and course id in request url
+//returns status 200 with updated course listings for user
+//returns status 401 if request is not made by course admin
+//returns status 500 for server or database error
 router.delete('/quiz/:quizId/:courseId', passport.authenticate('bearer', {session:false}), function(req, res) {
   var courseInToken = req.user.courses.find(function(course) {
     if(course._id === req.params.courseId) return true;
@@ -495,6 +550,11 @@ router.delete('/quiz/:quizId/:courseId', passport.authenticate('bearer', {sessio
     })
 });
 
+//records quiz submission by user
+//requires quizData (quiz submision), quiz id, user id in request body
+//calculates and saves submission score and date into entry
+//returns status 200 with quiz score, submission and passed (boolean, depreciated client side)
+//returns status 500 for server or database error
 router.post('/quiz/submit', passport.authenticate('bearer', {session:false}), jsonParser, function(req, res) {
   // score
   let quizData = req.body.quizData;
@@ -550,6 +610,10 @@ router.post('/quiz/submit', passport.authenticate('bearer', {session:false}), js
   })
 })
 
+//queries list of lessons in BOX folder
+//currently hardcoded folder, will need to implement course id or folder id in request url
+//returns status 200 with entries found by BOX (contains lesson name, id, modified time, size, url and sync state)
+//returns status 500 for server or BOX sdk error
 router.get('/lessons',
   passport.authenticate('bearer',
   {session:false}),
@@ -574,6 +638,10 @@ router.get('/lessons',
   );
 })
 
+//fetchs BOX preview and download url of lesson pdf
+//requires BOX lesson id in request url
+//returns status 200 with selectedPdf preview (url), download (url), name and id
+//returns status 500 for server or BOX sdk error
 router.get(
   '/lessons/:id',
   passport.authenticate('bearer', {session:false}),
@@ -599,6 +667,8 @@ router.get(
     })
 })
 
+//serves client side assests, html, css and js
+//enables react-router to navigate in client-side app
 router.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
